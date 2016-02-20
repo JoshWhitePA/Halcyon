7a7a44ef9084c7aefa782c1f834245328d8fad27
JoshWhitePA <josh.white.pa@gmail.com>
2016-02-20 14:29:16 -0500

    initial

---
+++ Assets/_Scripts/FrogController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogController : MonoBehaviour {
+		public Camera cam;
+		public float maxWidth;
+		// Use this for initialization
+		void Start () {
+			if (cam == null){
+				cam = Camera.main;
+			}
+//			Vector3 upperCorner = new Vector3 (Screen.width,Screen.height,0.0f);
+//			Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+//			maxWidth = targetWidth.x;
+		}
+
+		// Update is called once per frame
+		void FixedUpdate () {
+//			Vector3 v3  = Input.mousePosition;
+//			v3.z = 10;
+//			v3 = cam.ScreenToWorldPoint (v3);
+//			DebugConsole.Log(v3.x.ToString(),"red");
+//			Vector2 targetPos = new Vector2 (v3.x,0.0f);
+//			GetComponent<Rigidbody2D>().MovePosition (targetPos); 
+		}
+
+//	public Vector2 CenterFromFrog(){
+//
+//
+//	}
+	}
---
+++ Assets/_Scripts/LilyPadController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LilyPadController : MonoBehaviour {
+	int emotion = 20;
+	int angleODangle;
+	private bool odd = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rocking (5);
+	}
+	void rocking(int emotion){
+		angleODangle = (int)Mathf.PingPong (Time.time, emotion);
+		if (odd) {
+			transform.Rotate (Vector3.right * -angleODangle);
+		} else {
+			transform.Rotate (Vector3.left * angleODangle);
+		}
+
+			setOdd (!getOdd());
+		
+		DebugConsole.Log(angleODangle.ToString(),"red");
+		DebugConsole.Log(getOdd().ToString(),"blue");
+
+
+	}
+
+
+	public void setOdd(bool val){
+		odd = val;
+	}
+	public bool getOdd(){
+		return odd;
+	}
+
+}
