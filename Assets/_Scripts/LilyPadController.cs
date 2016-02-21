bccc2d9001aa44bf4859db7c45774362689db61e
JoshWhitePA <josh.white.pa@gmail.com>
2016-02-20 21:35:51 -0500

    Got frog to bob like it's in water

--- Assets/_Scripts/LilyPadController.cs
+++ Assets/_Scripts/LilyPadController.cs
@@ -1,41 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class LilyPadController : MonoBehaviour {
-	int emotion = 20;
-	int angleODangle;
-	private bool odd = true;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		rocking (5);
-	}
-	void rocking(int emotion){
-		angleODangle = (int)Mathf.PingPong (Time.time, emotion);
-		if (odd) {
-			transform.Rotate (Vector3.right * -angleODangle);
-		} else {
-			transform.Rotate (Vector3.left * angleODangle);
-		}
+	float originalY;
 
-			setOdd (!getOdd());
-		
-		DebugConsole.Log(angleODangle.ToString(),"red");
-		DebugConsole.Log(getOdd().ToString(),"blue");
+	public float floatStrength = 1; // You can change this in the Unity Editor to 
+	// change the range of y positions that are possible.
 
-
+	void Start()
+	{
+		this.originalY = this.transform.position.y;
 	}
 
-
-	public void setOdd(bool val){
-		odd = val;
-	}
-	public bool getOdd(){
-		return odd;
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x,
+			originalY + ((float)Math.Sin(Time.time) * floatStrength),
+			transform.position.z);
 	}
 
 }
