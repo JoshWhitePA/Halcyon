dcc4e00faa234bd8b21ee1b29694fce24d23916d
JoshWhitePA <josh.white.pa@gmail.com>
2016-04-09 08:54:48 -0400

    Cant remember bro

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class Timer : MonoBehaviour {
+	public float timeLeft = 120.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find("GUITimer").GetComponent<GUIText>().text = Math.Round(timeLeft,0).ToString();
+		timeLeft -= Time.deltaTime;
+		if(timeLeft < 0)
+		{
+			
+		}
+	}
+}
